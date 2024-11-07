a6bfc2db7be2ee8cd334240061038af181826217
student <albisharafiova@yandex.ru>
2024-11-07 17:49:41 +0300

    ssecond

--- app/src/main/java/ru/example/myapplication/CustomViewModel.kt
+++ app/src/main/java/ru/example/myapplication/CustomViewModel.kt
@@ -1,5 +1,6 @@
 package ru.example.myapplication
 
+import android.widget.Toast
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
@@ -7,43 +8,52 @@ class CustomViewModel: ViewModel() {
 
     val repo = ListRepoitory()
     val mutableLiveDataList = MutableLiveData<ArrayList<Film>>()
-    val mutableLiveDataTextList = MutableLiveData<String>()
+    val mutableLiveDataTextList = MutableLiveData<String>().apply {
+        value = repo.getList().toString()
+    }
     val mutableLiveDataKeyWord = MutableLiveData<String>()
-
+    var filterMode = "Years"
     fun search(){
-        val keyword = mutableLiveDataKeyWord.value
-        mutableLiveDataList.value = keyword?.let { repo.getList().filterListYears(it) }
-        mutableLiveDataTextList.value = mutableLiveDataList.value.toString()
+        if(filterMode.equals("Years")) {
+            val keyword = mutableLiveDataKeyWord.value
+            mutableLiveDataList.value = keyword?.let { repo.getList().filterListYears(it) }
+            mutableLiveDataTextList.value = mutableLiveDataList.value.toString()
+        }
+        else {
+            val keyword = mutableLiveDataKeyWord.value
+            mutableLiveDataList.value = keyword?.let { repo.getList().filterListTitle(it) }
+            mutableLiveDataTextList.value = mutableLiveDataList.value.toString()
+        }
+
     }
     fun ArrayList<Film>.filterListTitle( keyword:String):ArrayList<Film>{
         return this.filter { it -> it.title.lowercase().startsWith(keyword.lowercase())} as ArrayList<Film>
 
 
     }
-    fun ArrayList<Film>.filterListYears( keyword:String): ArrayList<Film>? {
-        return this.filter{it ->
-            val char = keyword[0]
-            if(char=='>') {
-                if (keyword.split(">")[1].length != 0) {
-                    return@filter (it.year > (keyword.split(">")[1].toInt()))
+    fun ArrayList<Film>.filterListYears( keyword:String): ArrayList<Film> {
+        return this.filter { it ->
+            if (keyword.length > 0) {
+                val char = keyword[0]
+                if (char == '>') {
+                    if (keyword.split(">")[1].length != 0) {
+                        return@filter (it.year > (keyword.split(">")[1].toInt()))
+                    } else {
+                        return ArrayList<Film>()
+                    }
+                } else {
+                    if (keyword.split("<")[1].length != 0) {
+                        return@filter (it.year < (keyword.split("<")[1].toInt()))
+                    } else {
+                        return ArrayList<Film>()
+                    }
                 }
-                else{
-                    return ArrayList<Film>()
-                }
-            }
-            else if(char == '<'){
-                if (keyword.split("<")[1].length != 0) {
-                    return@filter (it.year < (keyword.split("<")[1].toInt()))
-                }
-                else{
-                    return ArrayList<Film>()
-                }
+
             }
             else{
                 return ArrayList<Film>()
-
             }
-        }  as ArrayList<Film>
+        }as ArrayList<Film>
     }
     fun ArrayList<Film>.sortListDirector( keyword:String):ArrayList<Film>{
         //> <  лексико-графически сортировать
@@ -55,5 +65,13 @@ class CustomViewModel: ViewModel() {
             return this.sortedByDescending{it -> it.director}.toList() as ArrayList<Film>
         }
 
+    }
+    fun ChangeFilter() = if(filterMode.equals("Years")){
+        filterMode = "Titles"
+
+
+    }
+    else{
+        filterMode = "Years"
     }
 }
